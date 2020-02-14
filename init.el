7342db37cd7707f898c1afeb766cb0b69d7cd8e6
Sergey Korolev <korolev.srg@gmail.com>
2020-02-14 03:25:11 +0300

    chore: move aggressive-mode config in init

--- init.el
+++ init.el
@@ -1930,6 +1930,37 @@ If you experience stuttering, increase this.")
            "S" 'evil-surround-region
            "gS" 'evil-Surround-region))
 
+;; Aggressive Indent
+;; Minor mode to aggressively keep your code always indented.
+
+
+(use-package aggressive-indent
+  :diminish
+  :hook ((after-init . global-aggressive-indent-mode)
+         ;; FIXME: Disable in big files due to the performance issues
+         ;; https://github.com/Malabarba/aggressive-indent-mode/issues/73
+         (find-file . (lambda ()
+                        (if (> (buffer-size) (* 3000 80))
+                            (aggressive-indent-mode -1)))))
+  :config
+  ;; Disable in some modes
+  (dolist (mode '(asm-mode web-mode html-mode css-mode go-mode prolog-inferior-mode))
+    (push mode aggressive-indent-excluded-modes))
+
+  ;; Disable in some commands
+  (add-to-list 'aggressive-indent-protected-commands #'delete-trailing-whitespace t)
+
+  ;; Be slightly less aggressive in C/C++/C#/Java/Swift
+  (add-to-list
+   'aggressive-indent-dont-indent-if
+   '(and (or (derived-mode-p 'c-mode)
+             (derived-mode-p 'c++-mode)
+             (derived-mode-p 'csharp-mode)
+             (derived-mode-p 'java-mode)
+             (derived-mode-p 'swift-mode))
+         (null (string-match "\\([;{}]\\|\\b\\(if\\|for\\|while\\)\\b\\)"
+                             (thing-at-point 'line))))))
+
 ;; Flycheck
 ;; On-the-fly syntax checker.
 
@@ -1990,7 +2021,6 @@ If you experience stuttering, increase this.")
 ;; Old init file
 
 ;; Programming
-(require 'init-aggressive-indent)
 (require 'init-yasnippet)
 (require 'init-direnv)
 (require 'init-nix)
